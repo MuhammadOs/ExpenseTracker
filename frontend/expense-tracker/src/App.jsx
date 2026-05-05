28d6eb1058eac918206a6cb899686f5645287529
Muhammad Osama <muhammad2372002@gmail.com>
2026-05-05 11:27:17 +0300

    first commit

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,41 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+import Login from "./pages/Auth/Login"
+import SignUp from "./pages/Auth/SignUp"
+import Home from "./pages/Dashboard/Home"
+import Income from "./pages/Dashboard/Income"
+import Expense from "./pages/Dashboard/Expense"
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signUp" element={<SignUp />}></Route>
+          <Route path="/dashboard" element={<Home />}></Route>
+          <Route path="/income" element={<Income />}></Route>
+          <Route path="/expense" element={<Expense />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
+
+const Root = () => {
+  //check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+  
+  return isAuthenticated ? 
+  <Navigate to="/dashboard"/> 
+  : (
+    <Navigate to="/login"/>
+  );
+};
